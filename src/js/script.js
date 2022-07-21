3e7122845593123eb3e82cee3c1e3a10ef2c19b5
TheGiantNinja <yasserfedsi34@gmail.com>
2022-07-21 20:24:50 +0200

    Initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -207,6 +207,6 @@ function startTimerLine(time){
 
 function queCounter(index){
     //creating a new span tag and passing the question number and total question
-    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
+    let totalQueCounTag = '<span> <p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
     bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
 }
